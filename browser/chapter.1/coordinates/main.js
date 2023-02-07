d04c2a5fd6b77692ad95a0a32ae55b5d0d85b4d8
fnsl1026 <119789987+sangu1026@users.noreply.github.com>
2023-02-07 15:40:33 +0900

    좌표실습성능개선

--- browser/chapter.1/coordinates/main.js
+++ browser/chapter.1/coordinates/main.js
@@ -3,20 +3,25 @@ const vertical= document.querySelector('.vertical');
 const target= document.querySelector('.target');
 const tag= document.querySelector('.tag');
 
+const targetRect=target.getBoundingClientRect();
+console.log(targetRect);
+const targetHalfWidth=targetRect.width/2;
+const targetHalfHeight=targetRect.height/2;
+
+
 document.addEventListener('mousemove', (event)=>{
     var x=event.clientX;
     var y=event.clientY;
 
-    vertical.style.left=`${x}px`;
-    horizontal.style.top=`${y}px`;
+    vertical.style.transform=`translateX(${x}px)`;
+    horizontal.style.transform=`translateY(${y}px)`;
     
-    target.style.left=`${x}px`;
-    target.style.top=`${y}px`;
+    target.style.transform=`translate(${x-targetHalfWidth}px,${y-targetHalfHeight}px)`;
     
     tag.innerHTML=`${x}px,${y}px`;
-    tag.style.left=`${x}px`;
-    tag.style.top=`${y}px`;
-    hello;
+    tag.style.transform= `translate(${x+20}px,${y+20}px)`;
+    
+
 
     
 });
